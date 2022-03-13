6d149b4339a25b339e833e39c52303fd34e8564c
fauazi <101272857+fauazi@users.noreply.github.com>
2022-03-13 22:03:20 +0700

    Update test8.py

--- test8.py
+++ test8.py
@@ -1,4 +1,26 @@
-with open('rajarimba.txt', 'r') as f:
-    baca = f.read
-    for i in range(10):
-        print(baca)
+import threading
+import collections
+
+def baca_text(file):
+    buka = open(file)
+    baca = buka.read()
+    baca_split = baca.split()
+    ambil=collections.Counter(baca_split).most_common()
+    for kata in ambil:
+        tambah = open('iman.txt', 'a')
+        tambah.write("%s\t: %d\n"%(kata[0],kata[1]))
+
+files = [
+        '1.txt',
+        '2.txt',
+        '3.txt',
+        '4.txt',
+        '5.txt',
+        '6.txt',
+        '7.txt',
+        '8.txt',
+        '9.txt'
+]
+
+for file in files:
+   baca_text(file)
